973af436e874b0859ebf66aed81424f12ec22ac0
Ivan Neil Tapel <tapsivanneil@gmail.com>
2024-02-21 09:48:20 +0800

    add reset password capabilities to the website

--- config/mail.php
+++ config/mail.php
@@ -108,8 +108,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'captainteo0928@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'PostIt'),
     ],
 
     /*
